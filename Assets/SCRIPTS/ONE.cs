ede70ef02824252c2ebc3bfe4e68f87f53c289ed
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2023-03-06 11:24:38 +0100

    oneNOfet

--- Assets/SCRIPTS/ONE.cs
+++ Assets/SCRIPTS/ONE.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class ONE : MonoBehaviour
 {
-    
+    private int amountOfCubes;
+    private float randomScale;
+    private GameObject cube;
+
+    private void Start()
+    {
+        amountOfCubes = Random.Range(0, 30);
+        while (amountOfCubes > 0)
+        {
+            Instantiate();
+        }
+    }
+
+    private Vector3 randomPos()
+    {
+       position new Vector3 (Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10));
+        return 
+    }
 }
